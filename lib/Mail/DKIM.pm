19459ee3ebd3060e4478037cd3b5a25f63888708
Jason Long <jlong@messiah.edu>
2008-11-06 20:28:17 +0000

    update version numbers to reflect that current work is on 0.33
    git-svn-id: https://svn.code.sf.net/p/dkimproxy/code/Mail-DKIM/trunk@440 f38efd27-133c-0410-a3cc-a5f95e9cf04f

--- lib/Mail/DKIM.pm
+++ lib/Mail/DKIM.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 package Mail::DKIM;
-our $VERSION = 0.32;
+our $VERSION = 0.33;
 
 1;
 __END__
--- lib/Mail/DKIM/Common.pm
+++ lib/Mail/DKIM/Common.pm
@@ -15,7 +15,7 @@ use Mail::DKIM::Signature;
 package Mail::DKIM::Common;
 use base "Mail::DKIM::MessageParser";
 use Carp;
-our $VERSION = 0.32;
+our $VERSION = 0.33;
 
 sub new
 {
