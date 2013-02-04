ec520d1a917d08e01a554ca5b3241f062d027385
Jason Long <jlong@messiah.edu>
2013-02-04 16:18:54 +0000

    bugfix: avoid an 'uninitialized value' warning when signature being verified is missing a d= tag
    git-svn-id: https://svn.code.sf.net/p/dkimproxy/code/Mail-DKIM/trunk@694 f38efd27-133c-0410-a3cc-a5f95e9cf04f

--- lib/Mail/DKIM/Verifier.pm
+++ lib/Mail/DKIM/Verifier.pm
@@ -296,12 +296,17 @@ sub check_signature
 		return 0;
 	}
 
-	unless ($signature->domain ne '')
+	unless (defined $signature->domain)
 	{
 		# no domain specified
-		$self->{signature_reject_reason} =
-			!defined($signature->domain) ? "missing d tag"
-			: "invalid domain in d tag";
+		$self->{signature_reject_reason} = "missing d tag";
+		return 0;
+	}
+
+	if ($signature->domain eq '')
+	{
+		# blank domain
+		$self->{signature_reject_reason} = "invalid domain in d tag";
 		return 0;
 	}
 
